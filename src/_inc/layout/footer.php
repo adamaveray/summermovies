41f727527f00e0cbbe2606424c9cf48bcae6fc61
Adam Averay <adam@averay.com>
2016-04-25 21:02:55 +0800

    Updated email address

--- src/_inc/layout/footer.php
+++ src/_inc/layout/footer.php
@@ -1,7 +1,7 @@
 	<footer role="contentinfo">
 		<p>Made by <a href="http://adamaveray.com.au/" target="_blank">Adam Averay</a></p>
 		<p>Some data from <a href="http://www.omdbapi.com/" target="_blank" rel="nofollow">OMDb API</a> &amp; <a href="https://www.yahoo.com/news/weather/" target="_blank" rel="nofollow">Yahoo Weather</a></p>
-		<script>document.write('<p><'+'a  h'+(0 ? 'nul' : 'ref')+'="m'+'o,t,l,i,a'.split(',').reverse().join('')+':'+"Rr97&Wi100)Pc97.Vw109'Mn64%Ne97&Nt118(Et101$Vj114%Za97+Ps121'Wo46+Jd99,Gz111*Ol109$".replace(/[^a-zA-Z0-9]/g,';').replace(/[A-Z]/g,'&').replace(/[a-z]/g,'#')+'">'+"Contact"+'<\/a><\/p>');</script>
+		<script>document.write('<p><'+'a  h'+(0 ? 'nul' : 'ref')+'="m'+'o,t,l,i,a'.split(',').reverse().join('')+':'+"Nf99,Cy111'Si110-Vp116#Jq97.Hl99#Rn116.Fp64+Kk115*Lq117+Ge109.Zg109.Hz101%Cu114-Ee109(Bl111.Dc118'Zi105/Rt101'Fe115#Xk46%Vk110(Rs121(Wu99%".replace(/[^a-zA-Z0-9]/g,';').replace(/[A-Z]/g,'&').replace(/[a-z]/g,'#')+'">'+"Contact"+'<\/a><\/p>');</script>
 	</footer>
 
 	<?php /*
