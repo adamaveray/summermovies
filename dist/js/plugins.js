95522484458d24354f920a392128350c6647430f
Adam Averay <adam@averay.com>
2021-01-12 13:26:27 +0900

    Remove hardcoded dist files

--- dist/js/plugins.js
+++
@@ -1,1 +0,0 @@
-!function(){for(var e,i=function(){},n="assert,clear,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn".split(","),o=n.length,r=window.console=window.console||{};o--;)r[e=n[o]]||(r[e]=i)}();
